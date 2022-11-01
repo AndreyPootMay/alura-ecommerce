31396a83332c95e28e77852c5afc42e68d0e707d
AndreyPootMay <andreypootmay@gmail.com>
2022-10-31 21:50:51 -0600

    feat: :white_check_mark: First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Portafolio del bootcamp Alura One Latam 2022
+
+- Enlace del proyecto: <a href="https://andreypootmay.github.io/AluraGeek/" target="_blank">Ir a la tienda</a>
+
+<img src="https://drive.google.com/uc?export=view&id=1YaAZ03uTzKldWxH1EMTnQYWbghUv1TSf"/>
+
+
+## Para construir este portafolio, se utilizaron las siguientes tecnologías:
+- HTML
+- CSS
+- JavaScript
+
+## Notas del proyecto:
+- Capacidad de respuesta
+- Modularización HTML
+- Accesibilidad
+- Composición de páginas
+- Procesamiento JSON
+
+### Accesibilidad
+- La accesibilidad sigue casi todos los estándares de la extensión <b>herramienta de evaluación de olas</b>;
+- Durante el registro del Producto es posible insertar el texto Alternativa.
+
+
+### Más
+- Creación de un control deslizante, manualmente
---
+++ assets/js/navHeader/index.js
@@ -0,0 +1,82 @@
+
+const logo = (paths) => {
+  const logoLink = `
+  <a href="${paths.home}">
+      <img src="${paths.controle}" alt="imagem de um controle de videogame">
+      <img src="${paths.alura}" alt="alura">
+      <img src="${paths.geek}" alt="geek">
+  </a>
+  `
+  return logoLink;
+}
+
+
+export function nav(paths) {
+
+  const navHeader = document.querySelector('[data-nav]');
+  const logoLink = logo(paths)
+  const html = `
+  <div class="navHeader__left">
+    <div class="logo">
+      ${logoLink}
+    </div>
+    <div class="navHeader__search">
+      <input type="text" class="navHeader__search--input" placeholder="¿Qué desea encontrar?"/>
+      <img src="${paths.lupa}" alt="Imagen de una lupa" class="navHeader__search--img">
+    </div>
+  </div>
+  ${paths.login && (`<div class="navHeader__login"><a href="${paths.login}" class="btn__login">Login</a><img class="lupa__hidden" src="${paths.lupa}" alt="imagem de uma lupa" data-input/></div><input type="text" class="lupa__hidden--input" placeholder="¿Qué desea encontrar?"/>`)}
+  `;
+
+  navHeader.innerHTML = html;
+
+  if(paths.inputShow){
+    inputShow();
+  }
+
+}
+
+export function inputShow(){
+  const img = document.querySelector('[data-input]')
+  img.addEventListener('click',()=>{
+    document.querySelector('.lupa__hidden--input').classList.toggle('show');
+    document.querySelector('.lupa__hidden--input').focus();
+  })
+
+  document.querySelector('.lupa__hidden--input').addEventListener('blur',()=>{
+    document.querySelector('.lupa__hidden--input').classList.toggle('show');
+  })
+}
+
+
+export function footer(paths) {
+
+  const footer = document.querySelector('[data-footer]');
+  const logoLink = logo(paths)
+  const html = `
+  <section class="footer">
+    <div class="container">
+      <div class="logo">
+        ${logoLink}
+      </div>
+      <div class="footer__text">
+        <a href="#">Quienes somos</a href="">
+        <a href="#">Política de privacidad</a href="">
+        <a href="#">Finalidad del programa</a href="">
+        <a href="#">Anúnciese aquí</a href="">
+      </div>
+    </div>
+    <form class="footer__forms">
+      <p>Contacto</p>
+      <input type="text" placeholder="Escriba su nombre" required/>
+      <textarea placeholder="Escriba su mensaje" required></textarea>
+      <input type="submit" value="Enviar mensaje" class="btn__submit" data-submit />
+    </form>
+  </section>
+  <div class="footer__dev">
+  <p>Desarrollado por Niger Andrey Poot May</p>
+  </div>
+  `;
+
+  footer.innerHTML = html;
+}
